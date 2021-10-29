07f4272bc24d84b5446cb5e4aeb414d7b07c2cb3
Huan (李卓桓) <zixia@zixia.net>
2021-10-29 20:45:42 +0800

    fix smoke testing

--- tests/fixtures/smoke-testing.ts
+++ tests/fixtures/smoke-testing.ts
@@ -1,7 +1,7 @@
 #!/usr/bin/env -S node --no-warnings --loader ts-node/esm
 import {
   VERSION,
-}                       from 'wechaty-voteout'
+}                       from 'wechaty-bot5-assistant'
 
 async function main () {
   if (VERSION === '0.0.0') {
