a32e03cbe1e26e55873a3cab5c4b71557ef955c6
Huan (李卓桓) <zixia@zixia.net>
2021-12-16 19:57:18 +0800

    enhance baby machine & unit tests

---
+++ src/mailbox/baby-machine.fixture.ts
@@ -0,0 +1,105 @@
+/* eslint-disable sort-keys */
+import {
+  createMachine,
+  actions,
+}                   from 'xstate'
+
+import * as mailbox from './mailbox.js'
+
+const states = {
+  awake    : 'baby/awake',
+  sleeping : 'baby/sleeping',
+} as const
+
+const types = {
+  SLEEP : 'baby/SLEEP',
+  // sleeping
+  DREAM : 'baby/DREAM',
+  CRY   : 'baby/CRY',
+  PEE   : 'baby/PEE',
+  // awake
+  PLAY : 'baby/PLAY',
+  REST : 'baby/REST',
+  EAT  : 'baby/EAT',
+} as const
+
+const events = {
+  SLEEP : (ms: number)  => ({ type: types.SLEEP, ms }),
+  // sleeping
+  DREAM : ()            => ({ type: types.DREAM     }),
+  CRY   : ()            => ({ type: types.CRY       }),
+  PEE   : ()            => ({ type: types.PEE       }),
+  // awake
+  PLAY  : ()            => ({ type: types.PLAY      }),
+  REST  : ()            => ({ type: types.REST       }),
+  EAT : ()            => ({ type: types.EAT      }),
+}
+
+type BabyContext = { ms?: number }
+type BabyEvent   = ReturnType<typeof events.SLEEP>
+
+const machine = createMachine<BabyContext, BabyEvent, any>({
+  context: {},
+  id: 'child',
+  initial: states.awake,
+  states: {
+    [states.awake]: {
+      entry: [
+        actions.log('states.awake.entry', 'BabyMachine'),
+        actions.sendParent(mailbox.events.IDLE()),
+        actions.sendParent(events.PLAY()),
+      ],
+      on: {
+        [types.SLEEP]: {
+          target: states.sleeping,
+          actions: [
+            actions.log((_, e) => `states.awake.on.sleep.actions ${JSON.stringify(e)}`, 'BabyMachine'),
+            actions.sendParent(events.REST()),
+          ],
+        },
+      },
+      exit: [
+        actions.log('states.awake.exit', 'BabyMachine'),
+        actions.sendParent(events.EAT()),
+      ],
+    },
+    [states.sleeping]: {
+      entry: [
+        actions.log((_, e) => `states.sleeping.entry ${JSON.stringify(e)}`, 'BabyMachine'),
+        actions.assign({ ms: (_, e) => e.ms }),
+        actions.sendParent(events.DREAM()),
+      ],
+      after: {
+        cryMs: {
+          actions: actions.sendParent(events.CRY()),
+        },
+        ms: states.awake,
+      },
+      exit: [
+        actions.log(_ => 'states.sleeping.exit', 'BabyMachine'),
+        actions.assign({ ms: _ => undefined }),
+        actions.sendParent(events.PEE()),
+      ],
+    },
+  },
+}, {
+  delays: {
+    cryMs: ctx => {
+      const ms = Math.floor(Number(ctx.ms) / 2)
+      console.info('BabyMachine preMs', ms)
+      return ms
+    },
+    ms: ctx => {
+      const ms = Number(ctx.ms)
+      console.info('BabyMachine ms', ms)
+      return ms
+    },
+  },
+})
+
+export {
+  events,
+  machine,
+  states,
+  types,
+}
---
+++ src/mailbox/baby-machine.spec.ts
@@ -0,0 +1,120 @@
+#!/usr/bin/env -S node --no-warnings --loader ts-node/esm
+/* eslint-disable sort-keys */
+
+import {
+  test,
+  sinon,
+}                   from 'tstest'
+
+import {
+  createMachine,
+  interpret,
+  StateFrom,
+}                   from 'xstate'
+
+import * as baby from './baby-machine.fixture.js'
+import * as mailbox from './mailbox.js'
+
+test('babyMachine smoke testing with sleeping under mock clock', async t => {
+  const sandbox = sinon.createSandbox({
+    useFakeTimers: true,
+  })
+
+  const parentMachine = createMachine({
+    id: 'parent',
+    initial: 'testing',
+    invoke: {
+      id: 'child',
+      src: baby.machine,
+      autoForward: true,
+    },
+    states: {
+      testing: {},
+    },
+  })
+
+  const interpreter = interpret(parentMachine)
+
+  const eventList: string[] = []
+  interpreter.onTransition(s => {
+    eventList.push(s.event.type)
+    //
+    // console.info('onTransition: ')
+    // console.info('  - states:', s.value)
+    // console.info('  - event:', s.event.type)
+    // console.info()
+  })
+
+  interpreter.start()
+
+  const getChildSnapshot: () => StateFrom<typeof baby.machine> = () => interpreter.getSnapshot.call(
+    interpreter.getSnapshot().children['child'],
+  ) as any
+
+  let snapshot = getChildSnapshot()
+  t.equal(snapshot.value, baby.states.awake, 'babyMachine initial state should be awake')
+  t.same(eventList, [
+    'xstate.init',
+    mailbox.types.IDLE,
+    baby.types.PLAY,
+  ], 'should have initial event list')
+
+  eventList.length = 0
+  interpreter.send(baby.events.SLEEP(10))
+  snapshot = getChildSnapshot()
+  t.equal(snapshot.value, baby.states.sleeping, 'babyMachine state should be sleeping')
+  t.equal(snapshot.context.ms, 10, 'babyMachine context.ms should be 10')
+  t.same(eventList, [
+    baby.types.SLEEP,
+    baby.types.EAT,
+    baby.types.REST,
+    baby.types.DREAM,
+  ], 'should have event list')
+
+  eventList.length = 0
+  interpreter.send(baby.events.SLEEP(100000))
+  snapshot = getChildSnapshot()
+  t.equal(snapshot.value, baby.states.sleeping, 'babyMachine state should be sleeping')
+  t.equal(snapshot.context.ms, 10, 'babyMachine context.ms should be 10 (new event has been dropped)')
+  t.same(eventList, [
+    baby.types.SLEEP,
+  ], 'should no more response when sleeping')
+
+  eventList.length = 0
+  await sandbox.clock.tickAsync(4)
+  snapshot = getChildSnapshot()
+  t.equal(snapshot.value, baby.states.sleeping, 'babyMachine state should be sleeping after 1st 4 ms')
+  t.equal(snapshot.context.ms, 10, 'babyMachine context.ms should be 10 (new event has been dropped) after 1st 4 ms')
+  t.same(eventList, [], 'should no more response after 1st 4 ms')
+
+  eventList.length = 0
+  await sandbox.clock.tickAsync(4)
+  snapshot = getChildSnapshot()
+  t.equal(snapshot.value, baby.states.sleeping, 'babyMachine state should be sleeping after 2nd 4 ms')
+  t.equal(snapshot.context.ms, 10, 'babyMachine context.ms should be 10 (new event has been dropped) after 2nd 4 ms')
+  t.same(eventList, [
+    baby.types.CRY,
+  ], 'should cry in middle night (after 2nd 4 ms)')
+
+  eventList.length = 0
+  await sandbox.clock.tickAsync(2)
+  snapshot = getChildSnapshot()
+  t.equal(snapshot.value, baby.states.awake, 'babyMachine state should be awake after sleep')
+  t.equal(snapshot.context.ms, undefined, 'babyMachine context.ms should be cleared after sleep')
+  t.same(eventList, [
+    baby.types.PEE,
+    mailbox.types.IDLE,
+    baby.types.PLAY,
+  ], 'should pee after night and start paly in the morning, with idle event (after sleep)')
+
+  console.info(eventList)
+  /**
+   * Huan(202112) xstate bug:
+   *  interpreter.stop() will stop parent first,
+   *    then if child has any `exit: sendParent(...)` actions,
+   *    will throw exception
+   */
+  // interpreter.stop()
+
+  sandbox.restore()
+})
