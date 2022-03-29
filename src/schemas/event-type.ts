dd716e05f8e7b40edf2ed5fa2f211162c05e84c1
Huan (李卓桓) <zixia@zixia.net>
2022-03-29 18:55:25 +0800

    wip

--- src/schemas/event-type.ts
+++ src/schemas/event-type.ts
@@ -1,3 +1,6 @@
 import type * as events from './events.js'
 
 export type Event = ReturnType<typeof events[keyof typeof events]>
+export type Events = {
+  [K in keyof typeof events]: ReturnType<typeof events[K]>
+}
