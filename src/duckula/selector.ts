8e08398f6d7bc9baa739a8016b3c01f33eaeca7d
Huan <zixia@zixia.net>
2022-04-15 11:45:13 +0800

    fix selector with union typings

---
+++ src/duckula/selector.ts
@@ -0,0 +1,46 @@
+/* eslint-disable no-redeclare */
+export function selector <
+ TKey extends string,
+ TVal extends any,
+ TMap extends { [key in TKey]: TVal },
+> (source: TMap): TMap
+
+export function selector <
+  TKey extends string,
+  TVal extends any,
+  TMap extends { [key in TKey]: TVal },
+  TSelectedKey extends TKey,
+> (source: readonly [ TMap, readonly TSelectedKey[] ]): { [K in TSelectedKey]: TMap[K] }
+
+/**
+ * Pick the selected key from the source
+ *
+ * @param source
+ * @returns
+ */
+export function selector <
+  TKey extends string,
+  TVal extends any,
+  TMap extends { [key in TKey]: TVal },
+  TSelectedKey extends TKey,
+> (
+  source: TMap | [ TMap, TSelectedKey[] ],
+) {
+
+  if (!Array.isArray(source)) {
+    return source
+  }
+
+  const [ map, keys ] = source
+
+  type SelectedMap = { [K in TSelectedKey]: TMap[K] }
+  const SelectedMap = keys.reduce(
+    (acc, cur) => ({
+      ...acc,
+      [cur]: map[cur],
+    }),
+    {},
+  ) as SelectedMap
+
+  return SelectedMap
+}
